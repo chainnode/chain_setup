80c20fbfd28c76f199064f96676106e0e505eac3
Jun Yeon <mauver@naver.com>
2017-09-19 10:47:19 +0900

    update comment
    dd

--- ntp.sh
+++ ntp.sh
@@ -1,7 +1,9 @@
+cd chainpoint-node
 apt-get install ntp
 wget https://raw.githubusercontent.com/mauver/chainpoint_setup/master/ntp.conf
 cp ntp.conf /etc/ntp.conf
 systemctl restart ntp.service
-
+make down
+make up
 
 
