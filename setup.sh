bf1dd89fe734bf335b6b1fb18fa1c56050da83e8
chainnode <senagame1@gmail.com>
2017-09-23 17:48:57 +0000

    setup update

--- setup.sh
+++ setup.sh
@@ -7,6 +7,13 @@ fi
 
 ip=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
 
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
 curl -sSL https://cdn.rawgit.com/chainpoint/chainpoint-node/13b0c1b5028c14776bf4459518755b2625ddba34/scripts/docker-install-ubuntu.sh | bash
 cd ~/chainpoint-node
 echo "NODE_TNT_ADDRESS=$1" > .env
